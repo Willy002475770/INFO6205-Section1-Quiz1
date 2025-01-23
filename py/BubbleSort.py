d627312c1529837d7c77964153bb68426d0475ec
WEI CHENG TU <157180894+willy2828695@users.noreply.github.com>
2025-01-23 13:22:02 -0500

    updated

--- py/BubbleSort.py
+++ py/BubbleSort.py
@@ -8,6 +8,7 @@ def bubble_sort(arr):
     Returns:
         list: The sorted list.
     """
+    
     def swap(i, j):
         """
         Swap two elements in the list at indices i and j.
@@ -16,7 +17,23 @@ def bubble_sort(arr):
             i (int): Index of the first element.
             j (int): Index of the second element.
         """
+                    
         arr[i], arr[j] = arr[j], arr[i]
+    
+    boundary = -1  
+    while True:
+        swapped = False
+        i = 0
+            
+        while i < len(arr) - 1 and i+1 != boundary:
+            if arr[i] > arr[i+1]:
+                swap(i, i+1)
+                swapped = True
+            i += 1
+                
+        if not swapped:
+            break
+        boundary = i
 
-    # TODO: Implement the bubble sort algorithm below.
-    pass
+    return arr
+
